6eeb8c4fe6d797c0283a571862b01f4d42e44a75
dong-c-git <imdcc@sina.com>
2019-05-05 21:24:10 +0800

    20190506-commit

--- ex8.py
+++ ex8.py
@@ -2,3 +2,8 @@
 dayup = pow(1.001,365)
 daydown = pow(0.999,365)
 print("向上：{:.2f},向下：{:.2f}".format(dayup,daydown))
+#daydayup2.py
+dayfactor = 0.005
+dayup2 = pow(1+dayfactor,365)
+daydown2 = pow(1+dayfactor,365)
+print("向上：{:.2f},向下{:.2f}".format(dayup2,daydown2))

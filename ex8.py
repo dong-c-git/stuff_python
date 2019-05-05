9b9cfc74e9f7cd9acbe6865865cfa2f8e12b59fa
dong-c-git <imdcc@sina.com>
2019-05-05 21:29:59 +0800

    20190506-commit

--- ex8.py
+++ ex8.py
@@ -7,3 +7,12 @@ dayfactor = 0.005
 dayup2 = pow(1+dayfactor,365)
 daydown2 = pow(1+dayfactor,365)
 print("向上：{:.2f},向下{:.2f}".format(dayup2,daydown2))
+#daydayup3.py
+dayup3 = 1.0
+dayfactor3 = 0.01
+for i in range(365):
+    if i % 7 in[6,0]:
+        dayup3 = dayup3*(1-dayfactor3)
+    else:
+        dayup3 = dayup3*(1+dayfactor3)
+print("工作日的力量为:{:.2f}".format(dayup3))
